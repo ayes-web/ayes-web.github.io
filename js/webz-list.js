55499852ee6326b287c5c716ca50f28e51b45e2c
Ayes <52951851+ayes-web@users.noreply.github.com>
2020-12-22 18:01:31 +0200

    changed style

--- js/webz-list.js
+++ js/webz-list.js
@@ -1,49 +1,18 @@
 values = [
-["https://lewd.se/","90 day temporary file hosting."],
-["https://s-ul.eu/","Free file hosting with 1 free subdomain."],
-["https://privacytools.io/","Useful list of privacy tools."],
-["https://vgy.me/","Free image hosting."],
-["https://mullvad.net/","Recommended VPN service."],
-["https://matomo.org/","Google Analytics alternative."],
 ["https://froggi.es/mario/","Super Mario 64 on the Web!"],
-["https://noclip.website/","Explore many games on the web."],
-["https://spyware.neocities.org/index.html","Gives indepth ratings on spyware level of programs."],
-["https://web.obarun.org/","Arch Linux based system without systemd."],
-["http://www.bash.org/","Quore Databse."],
-["http://unicodeemoticons.com/","15000 Unicode symbols for emoticons from different languages and scripts."],
-["https://godotengine.org/"," Free and open source 2D and 3D game engine."],
 ["https://stumblingon.com/","Shows you a random webpage whenever you click the STUMBLE button."],
-["https://www.raylib.com/","raylib is a simple and easy-to-use library used to make games. Supports many languages and platforms."],
 ["https://copy.sh/v86/","Run multiple os in your browser."],
-["https://www.pfsense.org/","Open Source Firewall."],
 ["https://myspotify.pw/","Spotify stats."],
-["https://suckless.org/","Home of dwm, dmenu and other quality software."],
-["https://securitycheckli.st/","List of privacy tools. In my opinion has bad choices check out <a href='https://privacytools.io/'>privacytools.io</a> instead."],
-["https://authpass.app/","Open source keepass client for mobile."],
-["https://opensource.builders/","Helps find open-source alternatives for your favorite apps."],
-["https://prism-break.org/","Good list of privacy tools."],
 ["https://mailbox.org/en/","Paid email service with option for domain aliases."],
-["https://github.com/Hextris/Hextris","Fun open source game inspired by tetris."],
-["https://www.freedombox.org/","Personal server for locally hosting."],
-["https://yunohost.org/","Better personal server for locally hosting."],
-["https://librespeed.org/","Open Source Speedtest."],
-["https://switching.software/","Privacy friendly alternatives to software."],
-["https://lbry.tv/","Alternative to <a href='https://youtube.com/'>youtube</a>."],
+["https://yunohost.org/","Easy to use self hosting OS"],
 ["https://www.projecthoneypot.org/","Stops spammers."],
-["https://favicon.io/","Favicon generator."],
-["https://www.neoaxis.com/","Open source game engine."],
 ["https://twobithistory.org/","Blog about computer history intended primarily for computer people."],
-["https://dev.lemmy.ml/","Open source federated alternative to <a href='https://www.reddit.com/'>Reddit</a>."],
-["https://knockout.chat/","Cool forum."],
 ["http://www.plane9.com/","Music visualizer and sound responsive screensaver. Most likely abandoned by the dev."],
 ["https://15.ai/","Natural text-to-speech synthesis. Can do game character voices like glados from Portal"],
-["https://beam.cafe/","File sharing service."],
 ["https://mhitza.github.io/","Interesting posts on tech."],
-["https://carrd.co/","More limited version of <a href='https://neocities.org/'>Neocities</a>"],
-["https://shapez.io/","Open source <a href='https://factorio.com/'>Factorio</a> like game."],
+["https://shapez.io/","Open source Factorio like game."],
+["https://mullvad.net/","Good VPN service."],
 ];
-var empty = "﻿";
-var animation = ["Web sites","Web site","Web sit","Web si","Web s","Web ","We","W",empty,"W","We","Web","Web s","Web si","Web sit","Web site","Web sites"];
 function listLoad() {
     x = 0;
     for (i = 0; i < values.length; i++) {
@@ -80,14 +49,6 @@ function listLoad() {
     x += values[i];
   }
 }
-l = 0;
-setInterval(function(){
-    if (l >= animation.length) {
-      l = 0;
-    }
-    document.title = animation[l];
-    l += 1;
-  }, 500);
 window.onload = function() {
   listLoad()
   var elevator = new Elevator({
